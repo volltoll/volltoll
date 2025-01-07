61c810ddc5c852a24746aecaa571c0caf36478c2
volltoll8@gmx.de <volltoll8@gmx.de>
2025-01-07 16:00:52 +0100

    flex

--- special.js
+++ special.js
@@ -3,3 +3,12 @@ const listA = document.querySelector("#h678");
 listA.style.color = "red";
 
 console.log(listA);
+
+
+
+
+const unorderdList = document.querySelector("ul");
+unorderdList.style.display = "flex-end";
+console.log(unorderdList);
+
+
